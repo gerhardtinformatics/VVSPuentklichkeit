bf950ab0bb7b887ee676a09006ad059cdbf89af9
ls <ls@ls-debian>
2016-01-21 09:22:16 +0100

    Removed deprecated django.conf.urls.patterns(), use django.conf.urls.url() instead, added empty views.py file

--- vvs_crawler/vvs_crawler/urls.py
+++ vvs_crawler/vvs_crawler/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+from . import views
+
+urlpatterns = [
     # Examples:
     # url(r'^$', 'vvs_crawler.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+    
+    url(r'^admin/', admin.site.urls)
+    
+]
